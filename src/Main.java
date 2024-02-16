c1eac2a3cd6365ce84b214caa09ad109c294c7ea
adamd <adam.d.hinton@gmail.com>
2024-02-16 12:44:03 -0500

    successfully get user age from console

--- src/Main.java
+++ src/Main.java
@@ -14,10 +14,12 @@ public class Main {
     public static String getInputFromConsole (int currentYear){
 
         String name = System.console().readLine("Hi, what's your name? ");
-
         System.out.println("Hi, " + name + " thanks for taking the course");
 
-        return "";
+        String dateOfBirth = System.console().readLine("What year were you born?");
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+
+        return "So you are" + age + " years old";
     }
 
     public static String getInputFromScanner (int currentYear){
